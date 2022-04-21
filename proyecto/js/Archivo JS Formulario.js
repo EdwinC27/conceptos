832bef58ff6850805ed7f21b92f982e5c1a6ec61
EdwinC27 <edwin.a.castillo.27@gmail.com>
2022-04-21 14:30:02 -0500

    Subida de proyecto

---
+++ proyecto/js/Archivo JS Formulario.js
@@ -0,0 +1,151 @@
+function preguntas(){    
+    pregunta1();
+    pregunta2();
+    pregunta3();
+    pregunta4();
+    pregunta5();
+    pregunta6();
+    pregunta7();
+    pregunta8();
+    pregunta9();
+    pregunta10();
+    pregunta11();
+    pregunta12();
+    pregunta13();
+    pregunta14();
+    pregunta15();
+    pregunta16();
+    pregunta17();
+    pregunta18();
+    pregunta19();
+    pregunta20();
+}
+
+function pregunta1(){
+    let p1;
+    p1 = document.getElementById('p1').value;
+    alert("Lo que respondiste en la pregunta 1 es: "+p1);
+}
+
+function pregunta2(){
+    let p2;
+    p2 = document.getElementById('p2').value;
+    alert("Lo que respondiste en la pregunta 2 es: "+p2);
+}
+
+function pregunta3(){
+    let p3;
+    p3 = document.getElementById('p3').value;
+    alert("Lo que pusiste en la pregunta 3 es: "+p3);
+}
+
+
+function pregunta4(){
+    let p4;
+    p4 = document.getElementById('p4').value;
+    alert("Lo que pusiste en la pregunta 4 es: "+p4);
+}
+
+function pregunta5(){
+    let p5;
+    p5 = document.getElementById('p5').value;
+    alert("Lo que pusiste en la pregunta 5 es: "+p5);
+}
+
+function pregunta6(){
+    let p6;
+    p3 = document.getElementById('p6').value;
+    alert("Lo que pusiste en la pregunta 6 es: "+p6);
+}
+
+function pregunta7(){
+    let p7;
+    p7 = document.getElementById('p7').value;
+    alert("Lo que pusiste en la pregunta 7 es: "+p7);
+}
+
+function pregunta8(){
+    let p8;
+    p8 = document.getElementById('p8').value;
+    alert("Lo que pusiste en la pregunta 8 es: "+p8);
+}
+
+function pregunta9(){
+    let p9;
+    p9 = document.getElementById('p9').value;
+    alert("Lo que pusiste en la pregunta 9 es: "+p9);
+}
+
+function pregunta10(){
+    let p10;
+    p10 = document.getElementById('p10').value;
+    alert("Lo que pusiste en la pregunta 10 es: "+p10);
+}
+
+function pregunta11(){
+    let p11;
+    p11 = document.getElementById('p11').value;
+    alert("Lo qque pusiste en la pregunta 11 es: "+p11);
+}
+
+function pregunta12(){
+    let p12;
+    p12 = document.getElementById('p12').value;
+    alert("Lo que pusiste en la pregunta 12 es: "+p12);
+}
+
+function pregunta13(){
+    let p13;
+    p13 = document.getElementById('p13').value;
+    alert("Lo que pusiste en la pregunta 13 es: "+p13);
+}
+
+function pregunta14(){
+    let p14;
+    p14 = document.getElementById('p14').value;
+    alert("Lo que pusiste en la pregunta 14 es: "+p14);
+}
+
+function pregunta15(){
+    let p15;
+    p15 = document.getElementById('p15').value;
+    alert("Lo que pusiste en la pregunta 15 es: "+p15);
+}
+
+function pregunta16(){
+    let p16;
+    p16 = document.getElementById('p16').value;
+    alert("Lo que pusiste en la pregunta 16 es: "+p16);
+}
+
+function pregunta17(){
+    let p17;
+    p17 = document.getElementById('p17').value;
+    alert("Lo que pusiste en la pregunta 17 es: "+p17);
+}
+
+function pregunta18(){
+    let p18;
+    p18 = document.getElementById('p18').value;
+    alert("Lo que pusiste en la pregunta 18 es: "+p18);
+}
+
+function pregunta19(){
+    let p19;
+    p19 = document.getElementById('p19').value;
+    alert("Lo que pusiste en la pregunta 19 es: "+p19);
+}
+
+function pregunta20(){
+    let p20;
+    p20 = document.getElementById('p20').value;
+    alert("Lo que pusiste en la pregunta 20 es: "+p20);
+}
+
+function mostrar(){
+    let mostrar = prompt("Quieres ver tus respuestas: 1-Si  2-No: ");
+    if(mostrar==1){
+        preguntas();
+    }
+}
+
